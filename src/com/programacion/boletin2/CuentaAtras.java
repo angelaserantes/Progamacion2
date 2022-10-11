ac0fb348f9c16c45a7a7d49c44cedf41cfbb80b9
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-10-11 18:40:32 +0200

    cuenta atrás

---
+++ src/com/programacion/boletin2/CuentaAtras.java
@@ -0,0 +1,7 @@
+package com.programacion.boletin2;
+
+public class CuentaAtras {
+    public static void main(String[] args) {
+        System.out.println("Esto es una cuenta atrás 3,2,1,0");
+    }
+}
